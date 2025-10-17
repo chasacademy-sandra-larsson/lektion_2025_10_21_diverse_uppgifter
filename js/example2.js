7d22b26c3e163f95f06589a076431fdf4a23c885
chasacademy-sandra-larsson <111293794+chasacademy-sandra-larsson@users.noreply.github.com>
2025-10-17 10:13:09 +0200

    starter code

--- js/example2.js
+++ js/example2.js
@@ -1,15 +1,3 @@
 // Räkna antalet gånger ett visst element finns i en array
 
 const fruits  = ["orange", "apple", "banana", "kiwi", "orange"];
-
-let count = 0;
-
-const found = "kiwi";
-
-for(let i = 0; i < fruits.length; i++) {
-      if(fruits[i] === found) {
-        count += 1;
-      } 
-} 
-
-console.log(`Antalet ${found} är ${count}`);
